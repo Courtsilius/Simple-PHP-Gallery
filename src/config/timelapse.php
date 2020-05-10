72f34a48734738474faac407615bda961b8ab336
Pascal <callonz@protonmail.com>
2020-05-10 03:57:15 +0200

    Beta 1.3.1
    WIP commit

--- src/config/timelapse.php
+++ src/config/timelapse.php
@@ -5,7 +5,9 @@
 <script>
 
 <?php echo 'var images = ', json_encode($timelapseArray), ';'; ?>
+var playpause=0; //0 - Play, 1 - Pause
 var startID=0;
+var index=-1;
 function sleep(ms) {
   return new Promise(
     resolve => setTimeout(resolve, ms)
@@ -13,6 +15,7 @@ function sleep(ms) {
 }
 
 async function startTimelapse(index){
+	updatePlayPause();
 	startID++;
 	var i;
 	var startIndex
@@ -34,11 +37,46 @@ async function startTimelapse(index){
 	}
 }
 
-function advanceImage(index){
+function updatePlayPause(){
+	console.log(playpause);
+	if(playpause){
+		playpause=0;
+		document.getElementById("timelapseStartPausei").class="fa fa-play";
+		document.getElementById("timelapseStartPausebtn").onclick="startTimelapse(' + index + ' );";
+		return;
+	}
+	playpause=1;
+	document.getElementById("timelapseStartPausei").class="fa fa-pause";
+	document.getElementById("timelapseStartPausebtn").onclick="pauseTimelapse();";
+	
+
+}
+
+function updateImage(){
+	document.getElementById("timelapseimg").src=images[index];
+}
+function advanceImage(dir){
+	index = index + dir;
+	if(index > images.length-1){
+		index = images.length-1;
+	}
+	if(index < 0){
+		index=0;
+	}
+	updateImage();
 	
 	//TODO
 }
 
+function pauseTImelapse(){
+	startID++;
+}
+
+function stopTimelapse(){
+	index = 0;
+	startID++;
+	updateImage();
+}
 
 function openTimelapse(index){
     var height = $(window).height() * 0.8;
@@ -48,7 +86,7 @@ function openTimelapse(index){
     w2popup.open({
       title: 'Timelapse',
       body: '<div class="w2ui-centered"><img class="timg" id="timelapseimg" src="' + images[0] + '" style="max-width:' + imgwidth + 'px;max-height:' + imgheight + 'px;"></img></div>',
-	  buttons: 'Time between Pictures in ms: <input name="tbp" type="text" value="<?php echo $DEFAULTTIMELAPSEVALUE ?>" size="5"> - <button class="mediabtn" title="Go back (when on pause)" onclick="advanceImage(-1);"><i class="fa fa-step-backward" aria-hidden="true"></i></button><button class="mediabtn" title="Start Timelapse" id="timelapseStart" onclick="startTimelapse(' + index + ' );"><i class="fa fa-stop" aria-hidden="true"></i></button><button class="mediabtn" title="Start Timelapse" idid="timelapseStart" onclick="startTimelapse(' + index + ' );"><i class="fa fa-play" aria-hidden="true"></i></button><button class="mediabtn" title="Start Timelapse" id="timelapseStart" onclick="advanceImage(' + index + 1+ ' );"><i class="fa fa-step-forward" aria-hidden="true"></i></button> - <input type="radio" id="timelapseorder1" name="order" value="asc" checked> Ascending <input type="radio" id="timelapseorder2" name="order" value="des"> Descending',
+	  buttons: 'Time between Pictures in ms: <input name="tbp" type="text" value="<?php echo $DEFAULTTIMELAPSEVALUE ?>" size="5"> - <button class="mediabtn" title="Go back (when on pause)" onclick="advanceImage(-1);"><i class="fa fa-step-backward" aria-hidden="true"></i></button><button class="mediabtn" title="Stop Timelapse" onclick="stopTimelapse();"><i class="fa fa-stop" aria-hidden="true"></i></button><button class="mediabtn" title="Start Timelapse" id="timelapseStartPausebtn" onclick="startTimelapse(' + index + ' );"><i id="timelapseStartPausei" class="fa fa-play" aria-hidden="true"></i></button><button class="mediabtn" title="Go forward (when on pause)" onclick="advanceImage(1);"><i class="fa fa-step-forward" aria-hidden="true"></i></button> - <input type="radio" id="timelapseorder1" name="order" value="asc" checked> Ascending <input type="radio" id="timelapseorder2" name="order" value="des"> Descending',
 	  width: width,
 	  height: height
     });
